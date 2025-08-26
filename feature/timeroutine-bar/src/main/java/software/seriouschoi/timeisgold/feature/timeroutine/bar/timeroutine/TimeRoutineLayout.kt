30dd7ecf24427d052caff5ef93078ab07fc6fe33
seriouschoi <seriouschoi@gmail.com>
2025-08-26 15:33:39 +0900

    feat(stub): 타임 루틴 화면 개발

--- feature/timeroutine-bar/src/main/java/software/seriouschoi/timeisgold/feature/timeroutine/bar/timeroutine/TimeRoutineLayout.kt
+++ feature/timeroutine-bar/src/main/java/software/seriouschoi/timeisgold/feature/timeroutine/bar/timeroutine/TimeRoutineLayout.kt
@@ -3,8 +3,14 @@ package software.seriouschoi.timeisgold.feature.timeroutine.bar.timeroutine
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import java.time.DayOfWeek
 
 /**
@@ -18,9 +24,24 @@ fun TimeRoutineLayout(
 ) {
     val viewModel = hiltViewModel<TimeRoutineViewModel>()
 
+    val title by remember {
+        viewModel.uiState.map {
+            (it as? TimeRoutineUiState.Routine)?.title ?: ""
+        }.distinctUntilChanged()
+    }.collectAsState(initial = "")
+
+    LaunchedEffect(dayOfWeek) {
+        viewModel.readTimeRoutine(dayOfWeek)
+    }
+
     // TODO: jhchoi 2025. 8. 26. 구현.
+    /*
+    일단 루틴이 있나 없나 체크해야함.
+    루틴이 있으면, 루틴 이름을 보여주고, 없으면, 루틴을 만들까요? 라는 메시지를 보여줌.
+    이거를 uiState의 타입으로 만들자.
+     */
     Column(modifier = modifier) {
-        Text(text = dayOfWeek.toString())
+        Text(text = title)
     }
 
 }
