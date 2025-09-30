19075d645d8c54de5e0d0e8e2f28c9d84a007283
seriouschoi <seriouschoi@gmail.com>
2025-09-30 10:52:17 +0900

    chore: 슬롯 결과 표시 toast -> snackbar

--- feature/timeroutine/src/main/java/software/seriouschoi/timeisgold/feature/timeroutine/page/TimeRoutinePageScreen.kt
+++ feature/timeroutine/src/main/java/software/seriouschoi/timeisgold/feature/timeroutine/page/TimeRoutinePageScreen.kt
@@ -1,6 +1,5 @@
 package software.seriouschoi.timeisgold.feature.timeroutine.page
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,11 +7,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -52,24 +55,50 @@ fun TimeRoutinePageScreen(
         viewModel.load(dayOfWeek)
     }
 
-    val uiState by viewModel.uiState.collectAsState()
-    StateView(uiState) {
-        viewModel.sendIntent(it)
-    }
-    val uiEvent by viewModel.uiEvent.collectAsState(null)
-    EventView(uiEvent)
+    // TODO: jhchoi 2025. 9. 30. scaffolds 추가..
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackBarHostState) },
+        content = { innerPadding ->
+            Box(
+                Modifier.Companion
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                val uiState by viewModel.uiState.collectAsState()
+                StateView(uiState) {
+                    viewModel.sendIntent(it)
+                }
+                val uiEvent by viewModel.uiEvent.collectAsState(null)
+                EventView(uiEvent, snackBarHostState)
+            }
+        },
+        bottomBar = {
+
+        }
+    )
+
+
 }
 
 @Composable
-private fun EventView(event: Envelope<TimeRoutinePageUiEvent>?) {
-    when (val payload = event?.payload) {
-        is TimeRoutinePageUiEvent.ShowToast -> {
-            Toast.makeText(LocalContext.current, payload.message.asString(), payload.toastTime)
-                .show()
-        }
+private fun EventView(
+    event: Envelope<TimeRoutinePageUiEvent>?,
+    snackBar: SnackbarHostState
+) {
+    val context = LocalContext.current
+    LaunchedEffect(event) {
+        when (val payload = event?.payload) {
+            is TimeRoutinePageUiEvent.ShowToast -> {
 
-        null -> {
-            //no work.
+                val message = payload.message.asString(context)
+                snackBar.showSnackbar(message)
+            }
+
+            null -> {
+                //no work.
+            }
         }
     }
 }
@@ -79,7 +108,6 @@ private fun StateView(
     currentState: TimeRoutinePageUiState,
     sendIntent: (TimeRoutinePageUiIntent) -> Unit
 ) {
-
     when (currentState) {
         is TimeRoutinePageUiState.Loading -> {
             Loading(currentState)
