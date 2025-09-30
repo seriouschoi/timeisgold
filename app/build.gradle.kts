27e9f3a0128dd937692e70c696bb87f3d8327575
seriouschoi <seriouschoi@gmail.com>
2025-10-01 00:02:07 +0900

    build: 키스토어 설정 추가.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.timeisgold.android.application)
     alias(libs.plugins.timeisgold.android.compose)
@@ -11,6 +13,22 @@ android {
         applicationId = "software.seriouschoi.timeisgold"
         versionCode = 1
         versionName = "1.0"
+    }
+    signingConfigs {
+        getByName("debug") {
+            val props = Properties()
+            file("${rootDir}/keystore/keystore.properties").inputStream().use { props.load(it) }
+
+            storeFile = file(props.getProperty("DEBUG_STORE_FILE") as String)
+            storePassword = props.getProperty("DEBUG_STORE_PASSWORD") as String
+            keyAlias = props.getProperty("DEBUG_KEY_ALIAS") as String
+            keyPassword = props.getProperty("DEBUG_KEY_PASSWORD") as String
+        }
+    }
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
     lint {
         checkGeneratedSources = true
