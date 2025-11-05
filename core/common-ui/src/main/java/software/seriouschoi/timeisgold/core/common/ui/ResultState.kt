05aee5e5bdb9bd9c679ad93cf4927cb2a69c60e4
seriouschoi <seriouschoi@gmail.com>
2025-11-05 09:01:16 +0900

    doc: 이중랩핑 제거후 정리된 내용 주석 추가.

--- core/common-ui/src/main/java/software/seriouschoi/timeisgold/core/common/ui/ResultState.kt
+++ core/common-ui/src/main/java/software/seriouschoi/timeisgold/core/common/ui/ResultState.kt
@@ -4,12 +4,18 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onStart
 
+/**
+ * UI계층에서 사용할 상태 표현 객체.
+ */
 sealed class ResultState<out T> {
     object Loading : ResultState<Nothing>()
     data class Success<T>(val data: T) : ResultState<T>()
     data class Error(val throwable: Throwable) : ResultState<Nothing>()
 }
 
+/**
+ * flow의 생명주기를 상태로 전환.
+ */
 fun <T> Flow<ResultState<T>>.withResultStateLifecycle(): Flow<ResultState<T>> {
     return this
         .onStart { emit(ResultState.Loading) }
--- core/domain-data/src/main/java/software/seriouschoi/timeisgold/domain/data/DomainResult.kt
+++ core/domain-data/src/main/java/software/seriouschoi/timeisgold/domain/data/DomainResult.kt
@@ -3,30 +3,47 @@ package software.seriouschoi.timeisgold.domain.data
 /**
  * Created by jhchoi on 2025. 9. 2.
  * jhchoi
+ *
+ * 도메인 계층에서 사용할 상태표현 객체.
  */
 sealed class DomainResult<out T> {
     data class Success<T>(val value: T) : DomainResult<T>()
     data class Failure(val error: DomainError, val exception: Throwable? = null) : DomainResult<Nothing>()
 }
 
+/**
+ * 도메인 계층에서 분리가 완료된 에러 타입.
+ */
 sealed interface DomainError {
+    /**
+     * 유효성 검사 실패.
+     */
     sealed interface Validation : DomainError {
         data object EmptyTitle : Validation
         data object TitleLength : Validation
         data object NoSelectedDayOfWeek : Validation
     }
 
+    /**
+     * 데이터가 충돌하는 경우.
+     */
     sealed interface Conflict : DomainError {
         data object DayOfWeek : Conflict
         data object Data : Conflict
         data object Time : Conflict
     }
 
+    /**
+     * 데이터가 존재하지 않는 경우.
+     */
     sealed interface NotFound : DomainError {
         data object TimeRoutine : NotFound
         data object TimeSlot : NotFound
     }
 
+    /**
+     * 기타 기술적인 에러.
+     */
     sealed interface Technical : DomainError {
         data object Unknown : Technical
     }
